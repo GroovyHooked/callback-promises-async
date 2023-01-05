2ca17bb5fdbfcfcd8ba9c108a9cf124341108fef
GroovyHooked <thomas.cariot@dailymotion.com>
2023-01-05 09:08:33 +0100

    Refacto

--- callback.js
+++ callback.js
@@ -14,17 +14,17 @@ const service7_end = document.getElementById("service7-end");
 
 const timer = () => Math.random() * 10000;
 const dependencies = []
-let finalTime1 = 0;
-let finalTime2 = 0;
-//debugger
+let totalTime1 = 0;
+let totalTime2 = 0;
+
 const service1 = (callback) => {
   const time = timer();
-  const endTime = Math.round(time / 1000 * 100) / 100
-  endTime > finalTime1 ? finalTime1 = endTime : null;
+  const duration = Math.round(time / 1000 * 100) / 100
+  duration > totalTime1 ? totalTime1 = duration : null;
   console.log('Started service 1 at 0s');
   setTimeout(() => {
-    service1_end.innerHTML = endTime;
-    console.log('%cEnded: Service 1 at ' + endTime + ' s', 'color: red');
+    service1_end.innerHTML = duration;
+    console.log(`%cEnded: Service 1 at ${duration} s`, `color: red`);
     dependencies[0] = true
     if (dependencies[0] && dependencies[1]) {
       callback();
@@ -34,12 +34,12 @@ const service1 = (callback) => {
 
 const service2 = (callback) => {
   const time = timer();
-  const endTime = Math.round(time / 1000 * 100) / 100
-  endTime > finalTime1 ? finalTime1 = endTime : null;
+  const duration = Math.round(time / 1000 * 100) / 100
+  duration > totalTime1 ? totalTime1 = duration : null;
   console.log('Started service 2 at 0s');
   setTimeout(() => {
-    service2_end.innerHTML = endTime;
-    console.log('%cEnded: Service 2 at ' + endTime + 's', 'color: red');
+    service2_end.innerHTML = duration;
+    console.log(`%cEnded: Service 2 at ${duration}s`, 'color: red');
     dependencies[1] = true
     if (dependencies[0] && dependencies[1]) {
       callback();
@@ -49,12 +49,12 @@ const service2 = (callback) => {
 
 const service3 = (callback) => {
   const time = timer();
-  const endTime = Math.round(time / 1000 * 100) / 100
-  endTime > finalTime2 ? finalTime2 = endTime : null;
+  const duration = Math.round(time / 1000 * 100) / 100
+  duration > totalTime2 ? totalTime2 = duration : null;
   console.log('Started service 3 at 0s');
   setTimeout(() => {
-    service3_end.innerHTML = endTime;
-    console.log('%cEnded: Service 3 at ' + endTime + 's', 'color: blue');
+    service3_end.innerHTML = duration;
+    console.log(`%cEnded: Service 3 at ${duration} s`, 'color: blue');
     dependencies[2] = true
     if (dependencies[2] && dependencies[3]) {
       callback();
@@ -64,12 +64,12 @@ const service3 = (callback) => {
 
 const service4 = (callback) => {
   const time = timer();
-  const endTime = Math.round(time / 1000 * 100) / 100
-  endTime > finalTime2 ? finalTime1 = endTime : null;
+  const duration = Math.round(time / 1000 * 100) / 100
+  duration > totalTime2 ? totalTime1 = duration : null;
   console.log('Started service 4 at 0s');
   setTimeout(() => {
-    service4_end.innerHTML = endTime;
-    console.log('%cEnded: Service 4 at ' + endTime + 's', 'color: blue');
+    service4_end.innerHTML = duration;
+    console.log(`%cEnded: Service 4 at ${duration} s`, 'color: blue');
     dependencies[3] = true
     if (dependencies[2] && dependencies[3]) {
       callback();
@@ -79,14 +79,14 @@ const service4 = (callback) => {
 
 const service5 = () => {
   const time = timer();
-  const endTime = Math.round(time / 1000 * 100) / 100
-  service5_start.innerHTML = finalTime1;
-  console.log('%cService 1 & 2 ended at ' + finalTime1 + ', Starting Service 5', 'color: red');
-  const temp = finalTime1;
-  finalTime1 += endTime;
+  const duration = Math.round(time / 1000 * 100) / 100
+  service5_start.innerHTML = totalTime1;
+  console.log(`%cService 1 & 2 ended at ${totalTime1} , Starting Service 5`, 'color: red');
+  const temp = totalTime1;
+  totalTime1 += duration;
   setTimeout(() => {
-    service5_end.innerHTML = finalTime1;
-    console.log('%cService 5 started at ' + temp + 's, ended at ' + finalTime1 + 's' + ' and lasted ' + endTime + 's', 'color: red');
+    service5_end.innerHTML = totalTime1;
+    console.log(`%cService 5 started at ${temp}s, ended at ${totalTime1}s and lasted ${duration}s`, 'color: red');
     dependencies[4] = true
     if (dependencies[4] && dependencies[5]) {
       service7();
@@ -96,14 +96,14 @@ const service5 = () => {
 
 const service6 = () => {
   const time = timer();
-  const endTime = Math.round(time / 1000 * 100) / 100
-  service6_start.innerHTML = finalTime2;
-  console.log('%cService 3 & 4 ended at ' + finalTime2 + ', Starting Service 6', 'color: blue');
-  const temp = finalTime2;
-  finalTime2 += endTime;
+  const duration = Math.round(time / 1000 * 100) / 100
+  service6_start.innerHTML = totalTime2;
+  console.log(`%cService 3 & 4 ended at ${totalTime2} , Starting Service 6`, 'color: blue');
+  const temp = totalTime2;
+  totalTime2 += duration;
   setTimeout(() => {
-    service6_end.innerHTML = finalTime2;
-    console.log('%cService 6 started at ' + temp + 's, ended at ' + finalTime2 + 's' + ' and lasted ' + endTime + 's', 'color: blue');
+    service6_end.innerHTML = totalTime2;
+    console.log(`%cService 6 started at ${temp}s, ended at ${totalTime2}s and lasted ${duration}s`, 'color: blue');
     dependencies[5] = true
     if (dependencies[4] && dependencies[5]) {
       service7();
@@ -113,15 +113,15 @@ const service6 = () => {
 
 const service7 = () => {
   const time = timer();
-  const endTime = Math.round(time / 1000 * 100) / 100
-  let finaltime = finalTime1 > finalTime2 ? finalTime1 : finalTime2;
-  service7_start.innerHTML = finaltime;
-  console.log('%cService 5 & 6 ended at ' + finaltime + 's, Starting Service 7', 'color: green');
-  const temp = finaltime;
-  finaltime += endTime;
+  const duration = Math.round(time / 1000 * 100) / 100
+  let finalTime = totalTime1 > totalTime2 ? totalTime1 : totalTime2;
+  service7_start.innerHTML = finalTime;
+  console.log(`%cService 5 & 6 ended at ${finalTime} , Starting Service 7`, 'color: green');
+  const temp = finalTime;
+  finalTime += duration;
   setTimeout(() => {
-    service7_end.innerHTML = finaltime;
-    console.log('%cService 7 started at ' + temp + 's, ended at ' + finaltime + 's' + ' and lasted ' + endTime + 's', 'color: green');
+    service7_end.innerHTML = finalTime;
+    console.log(`%cService 7 started at ${temp}s, ended at ${finalTime}s and lasted ${duration}s`, 'color: green');
     dependencies[6] = true
   }, timer());
 }
