db14e1f16c78c099384a0890f5d67867ada09d2a
GroovyHooked <thomas.cariot@dailymotion.com>
2023-01-07 12:36:58 +0100

    Cleaner version of promises

--- promises.js
+++ promises.js
@@ -1,3 +1,7 @@
+const service1_start = document.getElementById("service1-start");
+const service2_start = document.getElementById("service2-start");
+const service3_start = document.getElementById("service3-start");
+const service4_start = document.getElementById("service4-start");
 const service1_end = document.getElementById("service1-end");
 const service2_end = document.getElementById("service2-end");
 const service3_end = document.getElementById("service3-end");
@@ -13,140 +17,57 @@ const service7_start = document.getElementById("service7-start");
 const service7_end = document.getElementById("service7-end");
 
 const timer = () => Math.random() * 10000;
-const dependencies = [];
+const dependencies = {
+  service1: false,
+  service2: false,
+  service3: false,
+  service4: false,
+  service5: false,
+  service6: false,
+};
 
-const service1 = () => {
+const service = (n, startTime, dep) => {
   const time = timer();
-  const duration = Math.round(time / 1000 * 100) / 100
+  const duration = Math.round(time / 1000 * 100) / 100;
   return new Promise((resolve) => {
     setTimeout(() => {
-      service1_end.innerHTML = `${duration}s`
-      dependencies[0] = true;
-      console.log(`service1: started at 0s and lasted ${duration}s`);
-      resolve(duration);
+      dep[`service${n}`] = true;
+      console.log(`service${n}: started at ${startTime}s and lasted ${duration}s for a total of ${startTime + duration}s`);
+      resolve(duration + startTime);
     }, time);
   });
 };
 
-const service2 = () => {
-  const time = timer();
-  const duration = Math.round(time / 1000 * 100) / 100
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      service2_end.innerHTML = `${duration}s`
-      dependencies[1] = true;
-      console.log(`service2: started at 0s and lasted ${duration}s`);
-      resolve(duration);
-    }, time);
+
+const runService = (n, startTime, deps) => {
+  service(n, startTime, deps).then((duration) => {
+    if (deps.service1 && deps.service2) {
+      deps.service1 = false;
+      deps.service2 = false;
+      service(5, duration, deps).then((duration) => {
+        if (deps.service5 && deps.service6) {
+          service(7, duration, deps)
+        }
+      })
+    }
+    if (deps.service3 && deps.service4) {
+      deps.service3 = false;
+      deps.service4 = false;
+      service(6, duration, deps).then((duration) => {
+        if (deps.service5 && deps.service6) {
+          service(7, duration, deps)
+        }
+      })
+    }
   });
-};
+}
 
-const service3 = () => {
-  const time = timer();
-  const duration = Math.round(time / 1000 * 100) / 100
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      service3_end.innerHTML = `${duration}s`
-      dependencies[2] = true;
-      console.log(`service3: started at 0s and lasted ${duration}s`);
-      resolve(duration);
-    }, time);
-  });
-};
 
-const service4 = () => {
-  const time = timer();
-  const duration = Math.round(time / 1000 * 100) / 100
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      service4_end.innerHTML = `${duration}s`
-      dependencies[3] = true;
-      console.log(`service4: started at 0s and lasted ${duration}s`);
-      resolve(duration);
-    }, time);
-  });
-};
+runService(1, 0, dependencies);
+runService(2, 0, dependencies);
+runService(3, 0, dependencies);
+runService(4, 0, dependencies);
 
-const service5 = (previousTime) => {
-  const time = timer();
-  const duration = Math.round(time / 1000 * 100) / 100
-  service5_start.innerHTML = `${previousTime}s`
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      service5_end.innerHTML = `${previousTime + duration}s`
-      dependencies[4] = true;
-      console.log(`service5 started at ${previousTime} and lasted ${duration}`);
-      const totalTime = previousTime + duration;
-      resolve(totalTime);
-    }, time);
-  });
-};
 
-const service6 = (previousTime) => {
-  const time = timer();
-  const duration = Math.round(time / 1000 * 100) / 100
-  service6_start.innerHTML = `${previousTime}s`
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      service6_end.innerHTML = `${previousTime + duration}s`
-      dependencies[5] = true;
-      console.log(`service6 started at ${previousTime} and lasted ${duration}`);
-      const totalTime = previousTime + duration;
-      resolve(totalTime);
-    }, time);
-  });
-};
 
-const service7 = (previousTime) => {
-  const time = timer();
-  const duration = Math.round(time / 1000 * 100) / 100
-  service7_start.innerHTML = `${previousTime}s`
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      service7_end.innerHTML = `${previousTime + duration}s`
-      console.log(`service7 started at ${previousTime}, lasted ${duration} and ended at ${previousTime + duration}s`);
-      const totalTime = previousTime + duration;
-      resolve(totalTime);
-    }, time);
-  });
-};
 
-service1().then(time => {
-  if (dependencies[0] && dependencies[1]) {
-    service5(time).then(time => {
-      if (dependencies[4] && dependencies[5]) {
-        service7(time);
-      }
-    });
-  }
-})
-
-service2().then(time => {
-  if (dependencies[0] && dependencies[1]) {
-    service5(time).then(time => {
-      if (dependencies[4] && dependencies[5]) {
-        service7(time);
-      }
-    })
-  }
-})
-
-service3().then(time => {
-  if (dependencies[2] && dependencies[3]) {
-    service6(time).then(time => {
-      if (dependencies[4] && dependencies[5]) {
-        service7(time);
-      }
-    })
-  }
-})
-
-service4().then(time => {
-  if (dependencies[2] && dependencies[3]) {
-    service6(time).then(time => {
-      if (dependencies[4] && dependencies[5]) {
-        service7(time);
-      }
-    })
-  }
-})
